f958b35ab1c74c7813b17e5b4911891c744422ca
wasabijiro <0xwasabijiro@gmail.com>
2025-05-04 20:01:24 +0900

    delete theme provider

--- apps/web/src/app/providers/Providers.tsx
+++ apps/web/src/app/providers/Providers.tsx
@@ -5,22 +5,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { networks } from "@workspace/sui";
 import { useState } from "react";
 import "@mysten/dapp-kit/dist/index.css";
-import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	const [queryClient] = useState(() => new QueryClient());
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ThemeProvider
-				defaultTheme="dark"
-				attribute="class"
-				enableColorScheme={false}
-			>
 				<SuiClientProvider networks={networks} defaultNetwork="testnet">
 					<WalletProvider>{children}</WalletProvider>
 				</SuiClientProvider>
-			</ThemeProvider>
 		</QueryClientProvider>
 	);
 }
