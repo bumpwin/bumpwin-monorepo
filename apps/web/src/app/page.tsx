41b59a4b6845a1e1f5db9855b633c4e89b7f2c58
wasabijiro <0xwasabijiro@gmail.com>
2025-05-04 22:11:10 +0900

    BattleClockコンポーネントを削除し、Headerコンポーネントに新しいナビゲーションとBattleClockを追加。スタイルを調整し、UIを改善。

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,9 @@
-import BattleClock from "@/components/BattleClock";
 import { ChampionCoinList } from "@/components/ChampionCoinList";
 import { CoinList } from "@/components/CoinList";
 
 export default function Home() {
 	return (
 		<div>
-			<BattleClock totalSeconds={15} challengeSeconds={5} />
 			<ChampionCoinList />
 			<CoinList />
 		</div>
